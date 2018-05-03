75af0c3ff3ecf259bb7160038e43b72158e1a268
Andrey Bodrov <secret@email.null>
2018-05-03 05:13:56 +0300

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+Just my Flow config
+====================
+
+# INTEGRATION:  
+* PyCharm https://www.jetbrains.com/help/pycharm/2016.3/using-the-flow-type-checker.html  
+* Atom: https://github.com/flowtype/ide-flowtype  
+
+# USAGE  
+As we added Glow, we should update `package.json` scripts as noticed [here](https://github.com/thejameskyle/glow#usage).  
+This package enhanced with auto-added pre-commit hook, so you need just to install it:  
+```
+yarn add -D flow-config
+```
+or
+```
+npm i -SD flow-config
+```
+
+For flow coverage report just add command to package.json scripts:
+```
+yarn flow-coverage-report -i 'src/**/*.js' -t html -t text
+```
+
+# CHANGELOG
+
+All notable changes to this project will be documented in this file.  
+This project adheres to [Semantic Versioning](http://semver.org/).  
+This change log adheres to standards from [Keep a CHANGELOG](http://keepachangelog.com).  
+
+### [0.1.0] - 2018-05-02
+* add flow-bin [0.70.0](https://github.com/facebook/flow/releases/tag/v0.70.0)
+* add flow-coverage-report [0.5.0](https://github.com/rpl/flow-coverage-report/tree/master#050)
+* add glow v1.2.2 (see https://medium.freecodecamp.org/3d469931eabb)
+* start with precommit hook checks only changed files (and all dependencies)
+* changelog whipped into shape (теперь всё как у взрослых)
---
+++ scripts/postinstall.js
@@ -0,0 +1,75 @@
+const jsonfile = require('jsonfile');
+const fs = require('fs');
+const process = require('process');
+
+const FLOW = 'flow';
+const FLOW_PRE = 'flow-precommit';
+const FLOW_TYPED = 'flow-typed';
+const PRE_COMMIT = 'pre-commit';
+
+try {
+    process.chdir('../../');
+    const packaged = jsonfile.readFileSync('package.json');
+    console.log(`Opened: ${packaged.name}@${packaged.version}`);
+
+    /**
+     * Добавляем в scripts
+     */
+    if (!packaged.scripts) packaged.scripts = {};
+    if (packaged.scripts[FLOW]) {
+        console.log(`Что-то уже добавлено в scripts.${FLOW}`);
+    } else {
+        packaged.scripts[FLOW] = './node_modules/flow-config/node_modules/.bin/flow';
+    }
+    if (packaged.scripts[FLOW_TYPED]) {
+        console.log(`Что-то уже добавлено в scripts.${FLOW_TYPED}`);
+    } else {
+        packaged.scripts[FLOW_TYPED] = './node_modules/.bin/flow-typed';
+    }
+    if (packaged.scripts[FLOW_PRE]) {
+        console.log(`Что-то уже добавлено в scripts.${FLOW_PRE}`);
+    } else {
+        packaged.scripts[FLOW_PRE] = 'bash ./node_modules/flow-config/scripts/pre-commit';
+    }
+
+    /**
+     * Регистрируем для пакета pre-commit
+     */
+    if (!(packaged[PRE_COMMIT] instanceof Array)) {
+        packaged[PRE_COMMIT] = [];
+    }
+    if (!packaged[PRE_COMMIT].includes(FLOW_PRE)) {
+        packaged[PRE_COMMIT].push(FLOW_PRE);
+    }
+
+    jsonfile.writeFileSync('package.json', packaged, {spaces: 2});
+    console.log('flow-config successfully added to package.json');
+
+    /**
+     * Создаю файл .flowconfig, если его ещё нет
+     */
+    if (!fs.existsSync('.flowconfig')) {
+        var rd = fs.createReadStream('./node_modules/flow-config-exness/.flowconfig');
+        rd.on("error", function (err) {
+            console.log(err);
+        });
+        var wr = fs.createWriteStream('.flowconfig');
+        wr.on("error", function (err) {
+            console.log(err);
+        });
+        wr.on("close", function (ex) {
+            console.log('.flowconfig was created');
+        });
+        rd.pipe(wr);
+
+    } else {
+        console.log('.flowconfig already exists');
+    }
+
+} catch (error) {
+    if (error.message === 'ENOENT, no such file or directory \'package.json\'') {
+        throw new Error('Нет доступа или нет package.json, прекоммит хук не добавлен');
+    } else {
+        console.log('Невалидный package.json, прекоммит хук не добавлен');
+    }
+}
